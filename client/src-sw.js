68f78c11c7d4124ba8b6114428b0b7457452aa9e
Arbitrary Stag <will.weaver.iv@gmail.com>
2023-06-24 18:33:19 -0700

    added code to src-sw.js file to manage the warmStrategyCache and offlineFallback strategy.

--- client/src-sw.js
+++ client/src-sw.js
@@ -5,7 +5,24 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+const cacheNames = {
+  pageCache: 'page-cache',
+  assetCache: 'asset-cache',
+};
+
 precacheAndRoute(self.__WB_MANIFEST);
+
+const assetCache = new CacheFirst({
+  cacheName: cacheNames.assetCache,
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 7 * 24 * 60 * 60, 
+    }),
+  ],
+});
 
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
@@ -25,6 +42,6 @@ warmStrategyCache({
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => !request.url.endsWith('/'), assetCache);
 
-// TODO: Implement asset caching
-registerRoute();
+offlineFallback();
